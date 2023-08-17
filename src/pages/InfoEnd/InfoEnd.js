42fff829a3b370daaa744ca1d95ae8367e55ebe2
Dima <dimsik3956525@gmail.com>
2023-08-17 15:20:57 +0600

    bag fix 3.0

--- src/pages/InfoEnd/InfoEnd.js
+++ src/pages/InfoEnd/InfoEnd.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './InfoEnd.css';
 const InfoEnd = () => (
-    <div className="infoEnd1">
+    <div className="infoEndH1">
         <div>
             <h1>Что вы получите после курса</h1>
         </div>
 
         <div className="infoEnd">
-            <div className="InfoendBlock">
+            <div>
                 <h1>Удостоверение</h1>
                 <span>Дающее право пеподовать робототехнику для детей 6-12 лет в оброзовательных учереждений</span>
             </div>
-            <div className="InfoendBlock">
+            <div>
                 <h1>Знание</h1>
                 <span>По основами разработки учебнометодических комплексов по робототехнике и прогромированию</span>
             </div>
-            <div className="InfoendBlock">
+            <div>
                 <h1>Практика</h1>
                 <span>Возможность пройти практику по преподованию робототехнике на базе R:ED LAB (в оффлайн иди ондайн формате)</span>
             </div>
